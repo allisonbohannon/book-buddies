195439b62ba258b3c2ee78d8064be75317b437d6
Allison Bohannon <akboh.github@gmail.com>
2023-04-04 20:58:09 -0700

    Add relationship for book / bookclub

--- app/serializers/book_serializer.rb
+++ app/serializers/book_serializer.rb
@@ -1,3 +1,6 @@
 class BookSerializer < ActiveModel::Serializer
   attributes :id, :title, :author, :published_date, :subject, :pages, :average_rating, :total_ratings, :cover_url
+
+  has_many :book_clubs
+  
 end

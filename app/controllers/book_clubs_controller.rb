c8c27c60d2e305fac021ff2d133f37a64721ae3e
Allison Bohannon <akboh.github@gmail.com>
2023-04-04 21:27:19 -0700

    Create methods

--- app/controllers/book_clubs_controller.rb
+++ app/controllers/book_clubs_controller.rb
@@ -1,2 +1,39 @@
 class BookClubsController < ApplicationController
+
+    def index 
+        books = BookClub.all 
+        render json: books  ##include: ['visits', 'visits.winery']
+    end
+
+    def create
+        bookclub = BookClub.create(bookclub_params)
+        render json: bookclub, status: :created
+    end
+
+    def show 
+        bookclub = find_bookclub
+        render json: bookclub
+    end
+
+    def destroy
+        bookclub = find_bookclub
+        bookclub.destroy
+        head :no_content
+    end
+
+
+    private 
+
+    def bookclub_params
+        params.permit(:book_id, :zip_three, :user_id, :status)
+    end
+
+    def find_bookclub
+        BookClub.find_by(id: params[:id])
+    end
+
+    def return_unprocessable_entity
+        render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+    end
+
 end
